9cee1cbdff48daa169d8b16c499a310a27c7d61d
Tristan <tristan.salles@sydney.edu.au>
2018-09-14 20:35:31 +1000

    add continental example

--- australia/run_espace.py
+++ australia/run_espace.py
@@ -1,8 +1,8 @@
 import argparse
-import gSCAPE as sim
+import eSCAPE as sim
 
 # Parsing command line arguments
-parser = argparse.ArgumentParser(description='This is a simple entry to run gSCAPE model.',add_help=True)
+parser = argparse.ArgumentParser(description='This is a simple entry to run eSCAPE model.',add_help=True)
 parser.add_argument('-i','--input', help='Input file name (YAML file)',required=True)
 parser.add_argument('-v','--verbose',help='True/false option for verbose', required=False,action="store_true",default=False)
 parser.add_argument('-l','--log',help='True/false option for PETSC log', required=False,action="store_true",default=False)
